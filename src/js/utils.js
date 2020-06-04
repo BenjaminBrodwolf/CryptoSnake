f760143b90d9d5862345e0b01ed2a1b893f04cdf
benjamin.brodolf@students.fhnw.ch <skaten88>
2020-06-05 00:10:56 +0200

    snake ready for online-test. Go for it

---
+++ src/js/utils.js
@@ -0,0 +1,34 @@
+const newcolor = (color1, color2, percentage) => {
+    color1 = color1 || '000000';
+    color2 = color2 || 'ffffff';
+    percentage = percentage || 0.5;
+
+    // console.log('valid: c1 => ' + color1 + ', c2 => ' + color2);
+
+    color1 = [parseInt(color1[0] + color1[1], 16), parseInt(color1[2] + color1[3], 16), parseInt(color1[4] + color1[5], 16)];
+    color2 = [parseInt(color2[0] + color2[1], 16), parseInt(color2[2] + color2[3], 16), parseInt(color2[4] + color2[5], 16)];
+
+    // console.log('hex -> rgba: c1 => [' + color1.join(', ') + '], c2 => [' + color2.join(', ') + ']');
+
+    let color3 = [
+        (1 - percentage) * color1[0] + percentage * color2[0],
+        (1 - percentage) * color1[1] + percentage * color2[1],
+        (1 - percentage) * color1[2] + percentage * color2[2]
+    ];
+
+   // console.log('c3 => [' + color3.join(', ') + ']');
+
+    color3 = int_to_hex(color3[0]) + int_to_hex(color3[1]) + int_to_hex(color3[2]);
+
+    // console.log(color3);
+
+    return color3;
+}
+
+
+const int_to_hex = num => {
+    let hex = Math.round(num).toString(16);
+    if (hex.length == 1)
+        hex = '0' + hex;
+    return hex;
+}
