41458c896f4dec3696610a82a4c0566459362e9c
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2020-06-06 14:27:47 +0200

    added abi

--- src/sol/snakeReproduction.sol
+++ src/sol/snakeReproduction.sol
@@ -5,8 +5,18 @@ import "./snakeCreator.sol";
 contract SnakeReproduction is SnakeCreator {
 
     using strings for *;
-    
+
     mapping(uint => uint[2]) public childToParent;
+    mapping(uint => address) snakeFoodToOwner;
+
+    string[] private snakeIncredients = ['Insects', 'Snails', 'Birds', 'Snakes', 'Deer'];
+
+    struct SnakeFood {
+        string name;
+        uint8 levelUp;
+    }
+
+    SnakeFood[] public snakeFoods;
 
     modifier onlyOwnerOf(uint _snakeId) {
         require(msg.sender == snakeToOwner[_snakeId]);
@@ -36,7 +46,8 @@ contract SnakeReproduction is SnakeCreator {
         string memory childName = sub1.toSlice().concat(sub2.toSlice());
 
         uint newDna = (mySourceSnake.dna + myTargetSnake.dna) / 2;
-        newDna = newDna - newDna % 100 + 42; //Marks the last 2 digits with 42 to identify the kind of a paired snake.
+        newDna = newDna - newDna % 100 + 42;
+        //Marks the last 2 digits with 42 to identify the kind of a paired snake.
 
         uint childId = _createSnake(childName, newDna);
         childToParent[childId] = [_sourceSnakeId, _targetSnakeId];
@@ -45,10 +56,27 @@ contract SnakeReproduction is SnakeCreator {
         _triggerCooldown(myTargetSnake);
     }
 
-    function feeding() internal pure {
-        //   TODO:
-        // 	- Schlange wird gefüttert. Schlangenfutter kaufen.
-        //  => wird stärker oder länger. => Wertsteigerung
+    function feeding(uint snakeFoodId, uint snakeId) internal {
+        require(snakeToOwner[snakeId] == msg.sender && snakeFoodToOwner[snakeFoodId] == msg.sender);
+
+        snakes[snakeId].level += snakeFoods[snakeFoodId].levelUp;
+        delete snakeFoodToOwner[snakeFoodId];
+    }
+
+    function buySnakeFood(string secretIngredient) external payable returns (uint) {
+        require(msg.value == 0.001 ether);
+        uint rand = uint(keccak256(abi.encodePacked(secretIngredient)));
+        uint8 levelup = uint8((rand % 5) + 1);
+        string memory ingredient = snakeIncredients[levelup];
+        SnakeFood memory snakeFood = SnakeFood(ingredient, levelup);
+        uint id = snakeFoods.push(snakeFood);
+        snakeFoodToOwner[id] = msg.sender;
+
+        return id;
+    }
+
+    function snakeFoodEquals(SnakeFood sf1, SnakeFood sf2) internal pure returns (bool){
+        return (keccak256(abi.encodePacked(sf1.name, sf1.levelUp)) == keccak256(abi.encodePacked(sf2.name, sf2.levelUp)));
     }
 
     //To connect the name of two snakes
@@ -61,15 +89,15 @@ contract SnakeReproduction is SnakeCreator {
 
         return string(result);
     }
-    
+
     function getNamesOfParents(uint childId) public view returns (string) {
         uint[2] memory parentsIDs = childToParent[childId];
         string memory snakeName1 = snakes[parentsIDs[0]].name;
         string memory snakeName2 = snakes[parentsIDs[1]].name;
-        
+
         string memory deliminator = ";";
 
         return snakeName1.toSlice().concat(deliminator.toSlice()).toSlice().concat(snakeName2.toSlice());
-        
+
     }
 }
