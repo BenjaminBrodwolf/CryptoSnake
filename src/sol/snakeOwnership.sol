f57c1314e57a06743834316dd7510b1a40a92870
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2020-06-09 18:09:33 +0200

    added snakeApproval

--- src/sol/snakeOwnership.sol
+++ src/sol/snakeOwnership.sol
@@ -9,6 +9,10 @@ contract SnakeOwnership is SnakeReproduction, ERC721 {
     using SafeMath for uint256;
 
     mapping(uint => address) snakeApprovals;
+    mapping(address => uint) receiverToApprovalsCount;
+    mapping(uint => uint) snakeToApprovedId;
+    
+    uint[] public approvedSnakes;
 
     function balanceOf(address _owner) external view returns (uint256) {
         return ownerSnakeCount[_owner];
@@ -27,13 +31,45 @@ contract SnakeOwnership is SnakeReproduction, ERC721 {
 
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
         require(snakeToOwner[_tokenId] == msg.sender || snakeApprovals[_tokenId] == msg.sender);
+        
+        if(snakeApprovals[_tokenId] == msg.sender){
+            delete snakeApprovals[_tokenId];
+            receiverToApprovalsCount[msg.sender] -= 1;
+            // TODO: remove snake id from array -> approved snakes: swap & delete
+            swapAndDelete(_tokenId);
+        }
+        
         _transfer(_from, _to, _tokenId);
     }
 
     function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
         snakeApprovals[_tokenId] = _approved;
+        receiverToApprovalsCount[_approved] += 1;
         emit Approval(msg.sender, _approved, _tokenId);
     }
     
- 
+    function swapAndDelete(uint snakeId) private {
+        uint index = snakeToApprovedId[snakeId];
+        uint lastElement = approvedSnakes[approvedSnakes.length - 1];
+        approvedSnakes[index] = lastElement;
+
+        delete approvedSnakes[approvedSnakes.length - 1];
+        approvedSnakes.length--;
+        // TODO: muss man das machen oder geschieht das automatisch ??
+        delete snakeToApprovedId[snakeId];
+        snakeToApprovedId[lastElement] = index;
+    }
+    
+    function getAllAprovedSnakes(address receiver) external view returns (uint[]) {
+       uint[] memory receivedSnakes = new uint[](receiverToApprovalsCount[receiver]);
+       
+        uint counter = 0;
+        for (uint i = 0; i < receivedSnakes.length; i++) {
+            if (snakeApprovals[approvedSnakes[i]] == receiver) {
+                receivedSnakes[counter] = approvedSnakes[i];
+                counter++;
+            }
+        }
+        return receivedSnakes;
+    }
 }
